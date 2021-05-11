eb3c40fbd160929ee60e3030ad41be1a517ae6c6
Karla Nieves <karlaxnieves@gmail.com>
2021-05-11 16:19:59 -0700

    got the list of movies to render

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
-const store = createStore();
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
